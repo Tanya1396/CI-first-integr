f01534a0323f7cdddf38a04759eb5d0d9a55afc4
Tatiana <tanya199613@gmail.com>
2019-11-17 21:28:31 +0500

    Modify: delete unnecessary import

--- src/test/java/ru/netology/CI/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/CI/CashBackHackServiceTest.java
@@ -1,6 +1,5 @@
 package ru.netology.CI;
 
-//import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
