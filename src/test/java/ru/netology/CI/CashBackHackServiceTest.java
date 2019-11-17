688aad1651e5d4c9e427729ac1f4463de6c252c5
Tatiana <tanya199613@gmail.com>
2019-11-17 20:59:46 +0500

    add Parametrized test

--- src/test/java/ru/netology/CI/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/CI/CashBackHackServiceTest.java
@@ -2,6 +2,8 @@ package ru.netology.CI;
 
 //import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,6 +29,14 @@ class CashBackHackServiceTest {
         CashBackHackService cash = new CashBackHackService();
         int actual = cash.remain(1000);
         int expected = 0;
+        assertEquals(expected,actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashBackHack.csv", numLinesToSkip = 1)
+    void remainParametrizedTest(int amount, int expected){
+        CashBackHackService cash = new CashBackHackService();
+        int actual = cash.remain(amount);
         assertEquals(expected,actual);
     }
 }
